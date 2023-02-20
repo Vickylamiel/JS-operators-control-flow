778f01b1f5a0119b73f5116aa51ac727fd6859eb
Vickylamiel <Ekehvictoria72@gmail.com>
2023-02-20 22:17:07 +0100

    Assignment on JavaScript Operators

--- index.js
+++ index.js
@@ -18,25 +18,38 @@ if (dept == 'Science') {
 
 // Question 5
 
-let pwr; 
-let firstValue; 
-const nearestPower = (num) => {
-  for (let i = 0; i < num; i++) {
-    pwr = 2 ** i;
-    if (pwr < num) {
-      firstValue = pwr;
-    }
-  }
-  pwr = num - firstValue > firstValue * 2 - num ? firstValue * 2 : firstValue;
-  console.log(pwr);
-  return `The number ${pwr} is the power of 2 nearest to ${num}`;
-};
-console.log(nearestPower(60));
+// let pwr; 
+// let firstValue; 
+// const nearestPower = (num) => {
+//   for (let i = 0; i < num; i++) {
+//     pwr = 2 ** i;
+//     if (pwr < num) {
+//       firstValue = pwr;
+//     }
+//   }
+//   pwr = num - firstValue > firstValue * 2 - num ? firstValue * 2 : firstValue;
+//   console.log(pwr);
+//   return `The number ${pwr} is the power of 2 nearest to ${num}`;
+// };
+// console.log(nearestPower(100));
 
 
 // const num = parseInt(60);
 // let pwr = Math.pow(2, Math.round(Math.log2(num)));
 
-// console.log(`The number ${pwr} is the power of 2 nearest to ${num}.`);
+
+function nearestPower (number) {
+  const log2 = Math.log2(number)
+  const round = Math.round(log2)
+  const pwr = Math.pow(2, round)
 
 
+  if (number >= 0) {
+      console.log(`The number ${pwr} is the power of 2 nearest to ${number}.`);
+  } else if (number < 0) {
+    console.log('Invalid number')
+  }
+
+}
+
+nearestPower(8)
